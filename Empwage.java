7a46d05b6f715972007e50bd46c0837cd2b5063d
Afrinfatma <afrinfatma4@gmail.com>
2022-06-27 12:16:28 +0530

    Added:Parttime wage added

--- Empwage.java
+++ Empwage.java
@@ -2,13 +2,17 @@ package com.bridgelabz.empwage;
 
 public class Empwage {
 	public static void main(String[] args){
-		int is_present=1;
+		int is_Fulltime=1;
+		int is_Parttime=2;
 		int Wageperhr=20;
 		int empwage=0 , emphrs=0;
-		double emp_check= Math.floor( Math.random()*10)%2;	
+		double emp_check= Math.floor( Math.random()*10)%3;	
 		 
-		if (emp_check==is_present)
+		if (emp_check==is_Fulltime)
 			emphrs=8;
+			
+		else if (emp_check==is_Parttime)
+			emphrs=4;
 			
 		else
 			emphrs=0;
