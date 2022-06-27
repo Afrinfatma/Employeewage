676e508073d7bf2768a416b412b3a8ebf1777ffa
Afrinfatma <afrinfatma4@gmail.com>
2022-06-27 12:37:42 +0530

    Refactor:Employee wage with switch case

--- Empwage.java
+++ Empwage.java
@@ -1,21 +1,24 @@
 package com.bridgelabz.empwage;
 
 public class Empwage {
+	public static final int is_Fulltime=1;
+	public static final int is_Parttime=2;
+	public static final int Wageperhr=20;
+	
 	public static void main(String[] args){
-		int is_Fulltime=1;
-		int is_Parttime=2;
-		int Wageperhr=20;
+		
 		int empwage=0 , emphrs=0;
-		double emp_check= Math.floor( Math.random()*10)%3;	
-		 
-		if (emp_check==is_Fulltime)
-			emphrs=8;
-			
-		else if (emp_check==is_Parttime)
-			emphrs=4;
-			
-		else
-			emphrs=0;
+		int emp_check=(int) Math.floor( Math.random()*10)%3;	
+		switch(emp_check) {
+		   case is_Fulltime :
+			   emphrs=8;
+		       break;
+		   case is_Parttime :
+			   emphrs=4;
+               break;
+           default:
+        	   emphrs=0;
+		}
 		empwage=Wageperhr*emphrs;
 		System.out.println("Employee wage:"+empwage);
 		}
