b7d02248362ec41b482005aa952360e35949d6b9
Afrinfatma <afrinfatma4@gmail.com>
2022-06-27 15:07:18 +0530

    Added :Employee wage using for loop for 20 WKD

--- Empwage.java
+++ Empwage.java
@@ -4,10 +4,11 @@ public class Empwage {
 	public static final int is_Fulltime=1;
 	public static final int is_Parttime=2;
 	public static final int Wageperhr=20;
-	
+	public static final int Num_of_working_days=20;
 	public static void main(String[] args){
 		
-		int empwage=0 , emphrs=0;
+		int empwage=0 , emphrs=0 , Totalempwage=0;
+		for(int day = 0; day < Num_of_working_days ; day++) {
 		int emp_check=(int) Math.floor( Math.random()*10)%3;	
 		switch(emp_check) {
 		   case is_Fulltime :
@@ -20,8 +21,10 @@ public class Empwage {
         	   emphrs=0;
 		}
 		empwage=Wageperhr*emphrs;
-		System.out.println("Employee wage:"+empwage);
+		Totalempwage+= empwage;
+		System.out.println("Employee wage: "+empwage);
 		}
+		System.out.println("Total Employee Wage: " +Totalempwage);
 	}
 
-
+}
