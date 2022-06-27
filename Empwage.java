7a63cdb5fa47cc0720a5a8ea23e869abdb01442c
Afrinfatma <afrinfatma4@gmail.com>
2022-06-28 00:47:36 +0530

    Refactor:Empwage for 100 working hrs and 20 working days

--- Empwage.java
+++ Empwage.java
@@ -5,25 +5,29 @@ public class Empwage {
 	public static final int is_Parttime=2;
 	public static final int Wageperhr=20;
 	public static final int Num_of_working_days=20;
+	public static final int Num_of_working_hrs=100;
 	public static void main(String[] args){
 		
-		int empwage=0 , emphrs=0 , Totalempwage=0;
-		for(int day = 0; day < Num_of_working_days ; day++) {
-		int emp_check=(int) Math.floor( Math.random()*10)%3;	
-		switch(emp_check) {
-		   case is_Fulltime :
+		
+		int  emphrs=0 , Totalemphrs=0, Totalworkingdays=0;
+		while(Totalemphrs<=Num_of_working_hrs && Totalworkingdays<Num_of_working_days) {
+			Totalworkingdays++;
+		    int emp_check=(int) Math.floor( Math.random()*10)%3;	
+		    switch(emp_check) {
+		       case is_Fulltime :
 			   emphrs=8;
 		       break;
-		   case is_Parttime :
+		       case is_Parttime :
 			   emphrs=4;
                break;
-           default:
+            default:
         	   emphrs=0;
 		}
-		empwage=Wageperhr*emphrs;
-		Totalempwage+= empwage;
-		System.out.println("Employee wage: "+empwage);
+		    Totalemphrs+= emphrs;
+		
+		System.out.println("Day#:" +Totalworkingdays + "Emp hrs:" +emphrs );
 		}
+		int Totalempwage= Totalemphrs * Wageperhr;
 		System.out.println("Total Employee Wage: " +Totalempwage);
 	}
 
